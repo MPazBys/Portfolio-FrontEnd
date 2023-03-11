114aaec91951f3d1fe8dc5bb3698778050a9dec4
MPazBys <pazbys25@gmail.com>
2023-03-10 23:07:46 -0300

    componente sobre mi

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
+import { SobremiComponent } from './componentes/sobremi/sobremi.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SobremiComponent
     ],
   imports: [
     BrowserModule,
